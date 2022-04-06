f005e51617598046e58ae070ab428ceb3e826bfa
joshop <joshop4646@gmail.com>
2022-04-06 12:08:22 -0400

    no idea what i changed

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-java -jar ../target/scala-2.12/millfork.jar -t nes_cnrom -Wextra-comparisons -s -fsource-in-asm -fsubroutine-extraction -finline -fipo -fillegals main.mfk -O$1
+java -jar ../target/scala-2.12/millfork.jar -t nes_cnrom -Wall -G ld65 -Wextra-comparisons -s -fsource-in-asm -fsubroutine-extraction -fdangerous-optimizations -finline -fipo -fillegals main.mfk -O$1

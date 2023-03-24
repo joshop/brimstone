88ce64d94a0adce7c9036daa49bc9a2f4870ae8d
joshop <joshop4646@gmail.com>
2023-03-24 17:30:42 -0400

    giga browser

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-java -jar ../target/scala-2.12/millfork.jar -t nes_cnrom -Wall -G ld65 -Wextra-comparisons -s -fsource-in-asm -fsubroutine-extraction -fdangerous-optimizations -finline -fipo -fillegals main.mfk -O$1
+java -jar ../target/scala-2.12/millfork.jar -t nes_nrom -Wall -G mesen -Wextra-comparisons -s -fsource-in-asm -fsubroutine-extraction -fdangerous-optimizations -finline -fipo -fillegals main.mfk -O$1
